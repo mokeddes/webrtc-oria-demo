94a469f5d8cb0886cce62b71fb933a9b6d8396df
Anis MOKEDDES <AMOKEDDES@externe.generali.fr>
2020-01-31 10:56:38 +0100

    feat(client): update stun server

--- public/client.js
+++ public/client.js
@@ -85,7 +85,35 @@ const handleLogin = async success => {
     document.querySelector("video#local").srcObject = localStream;
 
     const configuration = {
-      iceServers: [{ url: "stun:stun2.1.google.com:19302" }]
+      iceServers: [
+        { url: "stun:stun2.1.google.com:19302" },
+        { url: "stun:stun.stunprotocol.org" },
+        { url: "turn:numb.viagenie.ca" },
+        { url: "stun:stun01.sipphone.com" },
+        { url: "stun:stun.ekiga.net" },
+        { url: "stun:stun.fwdnet.net" },
+        { url: "stun:stun.ideasip.com" },
+        { url: "stun:stun.iptel.org" },
+        { url: "stun:stun.rixtelecom.se" },
+        { url: "stun:stun.schlund.de" },
+        { url: "stun:stun.l.google.com:19302" },
+        { url: "stun:stun1.l.google.com:19302" },
+        { url: "stun:stun2.l.google.com:19302" },
+        { url: "stun:stun3.l.google.com:19302" },
+        { url: "stun:stun4.l.google.com:19302" },
+        { url: "stun:stunserver.org" },
+        { url: "stun:stun.softjoys.com" },
+        { url: "stun:stun.voiparound.com" },
+        { url: "stun:stun.voipbuster.com" },
+        { url: "stun:stun.voipstunt.com" },
+        { url: "stun:stun.voxgratia.org" },
+        { url: "stun:stun.xten.com" },
+        {
+          url: "turn:numb.viagenie.ca",
+          username: "dojame2690@mailrunner.net",
+          credential: "password"
+        }
+      ]
     };
 
     connection = new RTCPeerConnection(configuration);
